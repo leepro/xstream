a64a9c5c32529b47285b88f25cc4c33ad98ffe81
DongWoo Lee <leepro@gmail.com>
2025-01-04 14:05:22 -0700

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/leepro/jstream
+
+go 1.23.4
---
+++ jstream.go
@@ -0,0 +1,9 @@
+// Copyright 2025 DongWoo Lee. All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+//
+// https://github.com/leepro/jstream
+
+// Package jstream provides data stream through websocket
+
+package jstream
